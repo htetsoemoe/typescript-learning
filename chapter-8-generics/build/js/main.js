e94092cd7872789116b7834a83391020336d4672
htetsoemoe <soemoehtet.mdy@gmail.com>
2023-12-23 15:53:08 +0630

    generics in typescript

---
+++ chapter-8-generics/build/js/main.js
@@ -0,0 +1,131 @@
+"use strict";
+const echo = (arg) => arg;
+const isObj = (arg) => {
+    return (typeof arg === 'object' && !Array.isArray(arg) && arg !== null);
+};
+console.log(isObj([1, 2, 3]));
+console.log(isObj(true));
+console.log(isObj('Dave'));
+console.log(isObj({ name: 'John' }));
+console.log(isObj(null));
+// **********************************
+const isTrue = (arg) => {
+    if (Array.isArray(arg) && !arg.length) {
+        return { arg, is: false };
+    }
+    if (isObj(arg) && !Object.keys(arg).length) {
+        return { arg, is: false };
+    }
+    return { arg, is: !!arg }; // !!false === false (or) !!true === true
+};
+console.log(isTrue(false));
+console.log(isTrue(0));
+console.log(isTrue(true));
+console.log(isTrue(1));
+console.log(isTrue('Dave'));
+console.log(isTrue(''));
+console.log(isTrue(null));
+console.log(isTrue(undefined));
+console.log(isTrue({})); // modified
+console.log(isTrue({ name: 'Dave' }));
+console.log(isTrue([])); // modified
+console.log(isTrue([1, 2, 3]));
+console.log(isTrue(NaN));
+console.log(isTrue(-0));
+const checkBoolValue = (arg) => {
+    if (Array.isArray(arg) && !arg.length) {
+        return { value: arg, is: false };
+    }
+    if (isObj(arg) && Object.keys(arg).length) {
+        return { value: arg, is: false };
+    }
+    return { value: arg, is: !!arg };
+};
+class Person {
+    constructor(id, name) {
+        this.id = id;
+        this.name = name;
+    }
+}
+const processUser = (user) => {
+    return user;
+};
+console.log(processUser({ id: 1, name: 'John' })); // object implements HasID interface
+const jack = new Person(2, 'Jack');
+const cherry = new Person(3, 'Cherry');
+console.log(processUser(jack));
+console.log(processUser(cherry));
+// *************************************
+// <Generic Type>(users: Generic Type): Return Type
+const getUsersProperty = (users, key) => {
+    return users.map(user => user[key]);
+};
+const usersArray = [
+    {
+        "id": 1,
+        "name": "Leanne Graham",
+        "username": "Bret",
+        "email": "Sincere@april.biz",
+        "address": {
+            "street": "Kulas Light",
+            "suite": "Apt. 556",
+            "city": "Gwenborough",
+            "zipcode": "92998-3874",
+            "geo": {
+                "lat": "-37.3159",
+                "lng": "81.1496"
+            }
+        },
+        "phone": "1-770-736-8031 x56442",
+        "website": "hildegard.org",
+        "company": {
+            "name": "Romaguera-Crona",
+            "catchPhrase": "Multi-layered client-server neural-net",
+            "bs": "harness real-time e-markets"
+        }
+    },
+    {
+        "id": 2,
+        "name": "Ervin Howell",
+        "username": "Antonette",
+        "email": "Shanna@melissa.tv",
+        "address": {
+            "street": "Victor Plains",
+            "suite": "Suite 879",
+            "city": "Wisokyburgh",
+            "zipcode": "90566-7771",
+            "geo": {
+                "lat": "-43.9509",
+                "lng": "-34.4618"
+            }
+        },
+        "phone": "010-692-6593 x09125",
+        "website": "anastasia.net",
+        "company": {
+            "name": "Deckow-Crist",
+            "catchPhrase": "Proactive didactic contingency",
+            "bs": "synergize scalable supply-chains"
+        }
+    },
+];
+console.log(getUsersProperty(usersArray, "name"));
+console.log(getUsersProperty(usersArray, "email"));
+// ****************************************
+class StateObject {
+    constructor(value) {
+        this.data = value;
+    }
+    get state() {
+        return this.data;
+    }
+    set state(value) {
+        this.data = value;
+    }
+}
+const storeObj1 = new StateObject("Cherry");
+console.log(`Getter: `, storeObj1.state);
+storeObj1.state = "Alice";
+// storeObj1.state = 33 // Type 'number' is not assignable to type 'string'
+const storeObj2 = new StateObject(33);
+console.log(`Getter: `, storeObj2.state);
+storeObj2.state = true;
---
+++ chapter-8-generics/src/main.ts
@@ -0,0 +1,169 @@
+const echo = <T>(arg: T): T => arg
+
+const isObj = <T>(arg: T): boolean => {
+    return (typeof arg === 'object' && !Array.isArray(arg) && arg !== null)
+}
+
+console.log(isObj([1, 2, 3]));
+console.log(isObj(true));
+console.log(isObj('Dave'));
+console.log(isObj({name: 'John'}));
+console.log(isObj(null));
+
+// **********************************
+
+const isTrue = <T>(arg: T): {arg: T, is: boolean} => {
+    if (Array.isArray(arg) && !arg.length) {
+        return {arg, is: false}
+    }
+    if (isObj(arg) && !Object.keys(arg as keyof T).length) {
+        return {arg, is: false}
+    }
+
+    return {arg, is: !!arg} // !!false === false (or) !!true === true
+}
+
+console.log(isTrue(false))
+console.log(isTrue(0))
+console.log(isTrue(true))
+console.log(isTrue(1))
+console.log(isTrue('Dave'))
+console.log(isTrue(''))
+console.log(isTrue(null))
+console.log(isTrue(undefined))
+console.log(isTrue({})) // modified
+console.log(isTrue({ name: 'Dave' }))
+console.log(isTrue([])) // modified
+console.log(isTrue([1, 2, 3]))
+console.log(isTrue(NaN))
+console.log(isTrue(-0))
+
+// *************************************
+
+interface BoolCheck<T> {
+    value: T,
+    is: boolean,
+}
+
+const checkBoolValue = <T>(arg: T): BoolCheck<T> => {
+    if (Array.isArray(arg) && !arg.length) {
+        return {value: arg, is: false}
+    }
+    if (isObj(arg) && Object.keys(arg as keyof T).length) {
+        return {value: arg, is: false}
+    }
+    return {value: arg, is: !!arg}
+}
+
+// *************************************
+
+interface HasID {
+    id: number
+}
+
+class Person implements HasID {
+    id: number;
+    name: string;
+
+    constructor(id: number, name: string) {
+        this.id = id;
+        this.name = name;
+    }
+}
+
+const processUser = <T extends HasID>(user: T): T => {
+    return user
+}
+
+console.log(processUser({id: 1, name: 'John'})); // object implements HasID interface
+
+const jack = new Person(2, 'Jack')
+const cherry = new Person(3, 'Cherry')
+console.log(processUser(jack));
+console.log(processUser(cherry));
+
+
+// *************************************
+// <Generic Type>(users: Generic Type): Return Type
+const getUsersProperty = <T extends HasID, K extends keyof T>(users: T[], key: K): T[K][] => {
+    return users.map(user => user[key])
+}
+
+const usersArray = [
+    {
+        "id": 1,
+        "name": "Leanne Graham",
+        "username": "Bret",
+        "email": "Sincere@april.biz",
+        "address": {
+            "street": "Kulas Light",
+            "suite": "Apt. 556",
+            "city": "Gwenborough",
+            "zipcode": "92998-3874",
+            "geo": {
+                "lat": "-37.3159",
+                "lng": "81.1496"
+            }
+        },
+        "phone": "1-770-736-8031 x56442",
+        "website": "hildegard.org",
+        "company": {
+            "name": "Romaguera-Crona",
+            "catchPhrase": "Multi-layered client-server neural-net",
+            "bs": "harness real-time e-markets"
+        }
+    },
+    {
+        "id": 2,
+        "name": "Ervin Howell",
+        "username": "Antonette",
+        "email": "Shanna@melissa.tv",
+        "address": {
+            "street": "Victor Plains",
+            "suite": "Suite 879",
+            "city": "Wisokyburgh",
+            "zipcode": "90566-7771",
+            "geo": {
+                "lat": "-43.9509",
+                "lng": "-34.4618"
+            }
+        },
+        "phone": "010-692-6593 x09125",
+        "website": "anastasia.net",
+        "company": {
+            "name": "Deckow-Crist",
+            "catchPhrase": "Proactive didactic contingency",
+            "bs": "synergize scalable supply-chains"
+        }
+    },
+]
+
+console.log(getUsersProperty(usersArray, "name"));
+console.log(getUsersProperty(usersArray, "email"));
+
+// ****************************************
+
+class StateObject<T> {
+    private data: T
+
+    constructor(value: T) {
+        this.data = value
+    }
+
+    get state(): T {
+        return this.data
+    }
+
+    set state(value: T) {
+        this.data = value 
+    }
+}
+
+const storeObj1 = new StateObject("Cherry")
+console.log(`Getter: `, storeObj1.state);
+storeObj1.state = "Alice"
+// storeObj1.state = 33 // Type 'number' is not assignable to type 'string'
+
+const storeObj2 = new StateObject<(string | number | boolean)>(33)
+console.log(`Getter: `, storeObj2.state);
+storeObj2.state = true
